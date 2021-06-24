946c696e3404112e635c0b8cbdc128c9be9a359b
Erwin Pranata <erwin.pranata@admedika.co.id>
2021-06-25 02:37:42 +0700

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App'
-import Calculate from './Calculate'
-
-ReactDom.render(<Calculate/>, document.getElementById('root'));
